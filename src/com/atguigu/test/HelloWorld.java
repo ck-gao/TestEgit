f255492276b3cb160afe2fd8e0ecb937843608a4
gaomingxu <644595049@qq.com>
2019-11-05 20:22:05 +0800

    change file

--- src/com/atguigu/test/HelloWorld.java
+++ src/com/atguigu/test/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("hello!");
-		System.out.println("new line!");
+		System.out.println("qweq line!");
 		System.out.println("aaaa line!");
 	}
 
